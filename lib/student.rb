cc7cdc917113eab5c35d6c3d5638be1a7e8ee19b
oumarichard <richard.ouma@student.moringaschool.com>
2022-10-26 15:38:35 +0300

    Completed assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+require 'pry'
+class Student < User
 
+  attr_accessor :knowledge
+
+  def initialize
+    super
+    @knowledge=[]
+  end
+
+  def learn(addst)
+    @knowledge = addst
+  end
 end
+# binding.pry
